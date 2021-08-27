a5a3c2e3e3e94556ff742f2601f638acaa1ffb3c
sumit001007 <sumitapp22@gmail.com>
2021-08-27 19:35:43 +0530

    all_mix_dart_programming

---
+++ bin/all_mix_tutorial.dart
@@ -0,0 +1,303 @@
+
+
+
+void main(){
+  // print('hello world');
+  // var set1 = <int>{1,2,3,4};
+  // print(set1);
+  // print(set1.runtimeType);
+
+  // var a = 10;
+  // print(a);
+
+  // var b = 20;
+  // print(b);
+
+  // double t = 5.5;
+  // print(t);
+
+  // double y = 5.0;
+  // print(y);
+
+  // String name = 'sumit';
+  // print(name);
+
+  // bool temp = true;
+  // bool temp1 = false;
+  // print(temp1);
+  // print(temp);
+
+  // var a = 10;
+  // print(a);
+
+  // print('THIS IS THE START OF THE PROGRAM');
+  // print('THIS IS THE START OF THE PROGRAM');
+
+  // String to integer.
+
+  // int a = 10;
+  // int b = int.parse('23');
+  // print(a + b);
+
+  
+  // Stirng to Double
+
+  // int a = 10;
+  // String b = 50.233.toString();
+  // print(b);
+  // print(b.runtimeType);
+
+
+  // String a = "It's Easy To Learn Dart Programming Language";
+  // print(a);
+
+  // Concatenation Operator
+
+  // String a = 'sumit';
+  // print('My name is ' +a);
+  // print('$a');
+
+  // String a = 'rahul';
+  // print('My name is $a');
+
+  // String b = 'sumit_kumar_sarkar';
+  // print('String Length is ${b.length}');
+
+  // NOW USING THE VAR DATA TYPE;
+
+  // var a = true;
+  // print(a);
+  // a = false;
+  // print(a);
+
+
+  // var a;
+  // a = false;
+  // print(a) ; 
+  // print(a.runtimeType);
+  // a = true;
+  // print(a.runtimeType);
+  // print(a);
+  // print(a.runtimeType);
+  // a = 'amit';
+  // print(a);
+  // print(a.runtimeType);
+  // a = 10;
+  // print(a);
+  // print(a.runtimeType);
+
+  // NOW WE ARE USING THE DYNAMIC DATA TYPE
+
+    // dynamic a;
+    // a = 10;
+    // print(a);
+    // print(a.runtimeType);
+
+  // NOW SEEING FOR THE CONST AND FINAL 
+
+  // final a = 10;
+  // print(a);
+  // print(a.runtimeType);
+  // const b = 20; 
+  // print(b);
+  // print(b.runtimeType);
+
+
+  // int x = 1 ,y = 2;
+
+  // final a = x;
+  // print(a);
+  // const b = 10;
+  // print(b);
+
+
+  // NOW WE ARE GOING TO SEE IF AND ELSE STATEMENT;
+
+  // int a = 10;
+
+  // if(a == 10){
+  //   print('The value of a is 10 and it is ture');
+  // }else{
+  //   print('The Value of a is not ture');
+  // }
+
+
+  // Now we are going to see if-else statement
+
+  // int a = 25;
+
+  // if (a < 10 ){
+  //   print('less than 10');
+  // }else if( a < 20 ){
+  //     print('less than 20');
+  // } if ( a < 30){
+  //   print('less than 30');
+  // } else if(a < 40){
+  //   print('less than 40');
+  // } else{
+  //   print('false');
+  // }
+
+
+  // NOW WE ARE GOING TO SEE FOR LOOP.
+
+  // for(var i = 1; i <= 5; i++){
+  //   print(i);
+  // }
+
+  // for(var x = 100; x < 100; x++){
+  //   print(x);
+  // }
+
+  // WE ARE GOING TO SEE NESTED LOOPS.
+
+  // for(var i = 0; i < 5; i++){
+  //   print('the value of i = $i');
+  //   for(var j = 0;j < 3; j++){
+  //   print('j = $j');
+  // } print('');
+  // }
+
+  // for(var a = 0; a < 3; a++){
+  //   print('the value of a is $a');
+  //       for(var b = 0; b < 2; b++){
+  //   print('the value of b is  $b');
+  //   } // for spacing the output so we added the empty space.
+  //   print('');
+  // }
+
+
+  //  NOW WE ARE LOOK FOR WHILE LOOP.
+
+  // var a = 0;
+  // while(a < 5){
+  //   print('the Value of a is $a');
+  //   a++;
+  // }
+   
+  // var k = 10;
+  // while(k < 10){
+  //   print('the value of k is $k');
+  //   k++;
+  // }
+    
+
+  // NOW WE ARE LOOKING FOR DO-WHILE 
+
+  // int i = 0;
+
+  // do{
+  //   print('Hello World');
+  //   i++;
+  //   } while(i < 10);
+
+  // atleast the code will execute once.
+
+  // var i = 10;
+
+  // do{
+  //   print('Hello World');
+  //   i++;
+  //   } while(i < 10);
+
+  // NOW WE ARE COMPARING THE TWO LOOPS 
+  // THAT IS WHILE AND DO WHILE LOOP
+
+  // print('while Loop:- \n');
+  // var i = 100;
+
+  // while(i < 100){
+  //   print('hello while loop');
+  // } i++;
+
+  
+  // var j = 200;
+  // do{
+  //   print('hello this is do-while loop');
+  //   j++;
+  //   }  while(j < 200);
+
+
+  // NOW WE ARE LOOKING FOR BREAK STATEMENT
+
+    // for(var i=0; i<5; i++){
+    //   if(i==2){
+    //     break;
+    //   }
+    //   print('i=$i');
+    // }
+  
+
+  // CONTINUE STATEMENT
+
+  // for(var i = 0; i < 5; i++){
+  //   if(i == 2){
+  //     break;
+  //   }
+  //   print('i = $i');
+  // }
+  // print('');
+  // for(var j = 0; j < 5; j++){
+  //   if(j == 2){
+  //     continue;
+  //   }
+  //   print('j = $j');
+  // }
+
+
+  // for(var i = 0; i < 5; i++){
+  //   if( i == 2){
+  //       break;
+  //   }
+  //   print('i is $i');
+  // } print('');
+
+  // for(var j = 0; j < 5; j++){
+  //   if(j == 2){
+  //     continue;
+  //   }print('j is $j');
+  // }
+
+  // NOW WE ARE GOING TO SEE SWITCH CASE
+
+  // var veichle = 'car';
+
+  // switch(veichle){
+  //   case 'car':
+  //   print('car');
+  //   break;
+  //   case 'truck':
+  //   print('turck');
+  //   break;
+  //   case 'bus':
+  //   print('bus');
+  //   break;
+  //   default:
+  //   print('not found what you are looking for');
+  //   break;
+  // }
+
+  var veichle = 'car';
+
+  switch(veichle){
+    case 'car':
+    print('car');
+    break;
+    case 'truck':
+    print('turck');
+    break;
+    case 'bus':
+    print('bus');
+    break;
+    default:
+    print('not found what you are looking for');
+    break;
+  }
+
+  
+
+  
+
+
+    
+}
---
+++ test/all_mix_tutorial_test.dart
@@ -0,0 +1,8 @@
+import 'package:all_mix_tutorial/all_mix_tutorial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
