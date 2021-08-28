1d1a2d67bf739cad3f5c493110a99f4105e2cceb
sumit001007 <sumitapp22@gmail.com>
2021-08-28 18:29:59 +0530

    completed day 1 and 2

---
+++ bin/2nd_day.dart
@@ -0,0 +1,268 @@
+
+
+void main(){
+  // print('hello world');
+
+    // int a = 10;
+    // print(a);
+
+
+  // double b = 5.5;
+  // print(b); 
+
+  // double b = 5.0;
+  // print(b);
+
+
+  // String b = 'sumit';
+  // print(b);
+
+  // bool temp = false;
+  // print(temp);
+
+  // NOW WE ARE GOING TO SEE VAR KEYWORD.
+
+  // var a = 10;
+  // print(a);
+
+  // int a = 10;
+  // print(a);  
+
+  // now we are going to see Conversion.
+
+  // int a = 10;
+  // int b = int.parse('20');
+  // print(a+b);
+
+  // string to double
+
+  // int a = 10;
+  // double b = double.parse('20.5');
+  // print(a + b);
+
+  // INT TO STRING
+  // String b = 10.toString();
+  // print(b);
+  // print(b.runtimeType);
+
+  // STRING INTERPOLATION
+
+  // String a = 'rahul';
+  // print('my name is ' +a);
+
+  // String length
+
+  // String b = 'Ankit';
+  // print('The String length is ${b.length}');
+
+  // now we are decalring the var without initialize
+
+  // var a;
+  // a = true;
+  // print(a);
+  // print(a.runtimeType);
+
+  // a = 10;
+  // print(a);
+  // print(a.runtimeType);
+
+  // a = 'sumit';
+  // print(a);
+  // print(a.runtimeType);
+
+  // a = 2.2;
+  // print(a);
+  // print(a.runtimeType);
+
+  // DYNAMIC KEYWORD WE ARE SEEING NOW.
+
+  // dynamic a = 10;
+  // print(a);
+  // print(a.runtimeType);
+  // a = false;
+  // print(a);
+  // print(a.runtimeType);
+  // after initialize the dynamic data 
+  //type still also accepting the other data type.
+
+  // just we are looking into const and final.
+
+  // final a = 10;
+  // print(a);
+  // const b = 5;
+  // print(b);
+
+  // const cannot be assigned to a variable.
+
+  // now we are looking for if and else if
+  
+
+  // int a = 20;
+
+  // if(a == 30){
+  //   print('this is true');
+  // } else {
+  //   print('this is false');
+  // }
+
+  // int a = 35;
+
+  // if(a < 40){
+  //     print('the answer is ture and it is less than 40');
+  // } else if(a < 10){
+  //   print('the answer is true and it is less than 10');
+  // } else if(a < 5){
+  //   print('this is ture and final statement');
+  // }
+
+  // now we are going to look into for loop
+
+  // for(var i = 19;i< 20;i++){
+  //   print('the value of i is $i');
+  // }
+
+  // nested for loop
+
+  // for(var i = 0; i< 3;i++){
+  //   print('the value of i is $i');
+  //   for(var y = 0; y<2;y++){
+  //     print('the value of y is $y');
+  //   }print('');
+  // }
+
+  // NOW WE ARE GOING TO SEE WHILE LOOP
+
+  // var a = 10;
+  // while(a < 11){
+  //   print('good job $a');
+  //   a++;
+
+  // Again we are going to see while loop
+
+  // int a = 10;
+  // while(a < 11){
+  //   print('the while loop is executed ');
+  //   a++;
+  // }
+
+  // NOW WE ARE GOING TO LOOK INTO DO-WHILE LOOP
+
+  // int a = 9;
+  // do{
+  //   print('hello world this is do while loop');
+  //   a++;
+  // }while(a < 11);
+  // for atleast it will print once.
+
+  // Break Statement
+
+  // for(int a = 0; a < 10; a++ ){
+  //   if(a == 2){
+  //     break;
+  //   }
+  //   print('a value is $a');
+  // }
+
+  // Continue Statement
+  // the number 3 is skipped
+  // for(int b = 0; b < 5; b ++){
+  //   if(b == 3){
+  //     continue;
+  //     // print('the value of b is $b');
+  //   } print('the value of b is $b');
+  // }
+
+  // SWITCH CASE WE ARE NOW GOING TO SEE
+
+  // dynamic b = 'Truck';
+  // switch(b){
+  //   case 'Truck':
+  //   print('Turck 2');
+  //   break;
+  //   case 'Lorry':
+  //   print('lorry');
+  //   break;
+  //   default:
+  //   print('end of program');
+  // }
+
+  //AGAIN WE ARE GOING TO SEE SWITCH CASE
+
+  // var c = 'train';
+  // switch(c){
+  //   case 'train':
+  //   print('Train is printed');
+  //   break;
+  //   case 'bus':
+  //   print('bus is printed');
+  //   break;
+  //   default:
+  //   print('it is not a valid name');
+  //   break;
+  // }
+
+  // FINALLY WE ARE ON SECOND DAY OF OUR DART PROGRAMMING
+  // CONDITIONAL STATEMENT
+
+  // var a = 1;
+  // (a > 4)? print('ture'): print('false');
+
+  // var b = 10;
+  // (b > 11)? print('True') : print('False');
+
+  // int d = 50;
+  // (d < 60)? print('ture'):print('false');
+
+  // ANOTHER CONDITIONAL STATEMENT
+  // ## COMPILER SCOLD ME DONT USE NULL MANUALLY
+  // var a , b = 5;
+  // var temp = a ?? b;
+  // print(temp);
+
+  //var b , c = 10;
+  // var temp1 = b ?? c;
+  // print(temp1);
+
+  // NOW WE ARE GOING TO SEE FOR IS AND IS! OPERATION
+
+  // var a = 'sumit';
+  // print(a is String);
+  // print(a is! String);
+
+  // var b = true;
+  // print(b is bool);
+
+  // var a = 10;
+  // print('is operator = ${a is int}');
+  // print('is operator = ${a is! int}');
+
+  // THE MAIN IMPORTANT CONCEPT WE ARE GOING TO LEARN IS FUNCTION
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
